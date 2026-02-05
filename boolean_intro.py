a559846aa9ebab02d141305bb432bab2ed07edb4
AltaiDinmukhamed <altai.alisher@mail.ru>
2026-02-05 17:50:40 +0500

    Add files via upload

---
+++ boolean_intro.py
@@ -0,0 +1,29 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+# Boolean variables
+is_student = True
+is_teacher = False
+
+print(is_student)   # True
+print(is_teacher)   # False
+
+
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
